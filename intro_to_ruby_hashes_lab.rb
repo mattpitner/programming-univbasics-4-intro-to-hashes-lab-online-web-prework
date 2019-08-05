1d4f34a0de045dc93cd13ee6be849bfe15a220b6
Matt Pitner <mattpitner09@gmail.com>
2019-08-05 20:26:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -40,7 +40,7 @@ def read_from_hash(hash, key)
   return hash[key]
 end
 
-ddef update_counting_hash(hash, key)
+def update_counting_hash(hash, key)
     # given a hash an a key as parameters, return an updated hash
     # if the provided key is not present in the hash, add it and assign it to the value of 1
     # if the provided key is present, increment its value by 1
@@ -53,18 +53,3 @@ ddef update_counting_hash(hash, key)
     end 
     return hash
   end
-
-  hash = { 
-    :apples => 3,
-    :oranges => 1
-  }ef update_counting_hash(hash, key)
-  # given a hash an a key as parameters, return an updated hash
-  # if the provided key is not present in the hash, add it and assign it to the value of 1
-  # if the provided key is present, increment its value by 1
-  if hash [key] == nil 
-    hash [key] = 1
-  elseif 
-    hash [key] = hash[key] + 1 
-  end 
-  return hash
-end
