db68fb2aafbd401db2eec04c30536bac583a64e9
Matt Pitner <mattpitner09@gmail.com>
2019-08-05 19:24:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,7 +37,7 @@ end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
-  return hash[]
+  return hash[key]
 end
 
 def update_counting_hash(hash, key)
